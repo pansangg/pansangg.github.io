993bbbe4609bddd699cbceb9d2eb9564fdbc0dd2
pansangg <xboxpolzovatel2012@gmail.com>
2025-04-16 02:14:09 +0700

    я устал помогите я спать

--- script.js
+++ script.js
@@ -10,6 +10,36 @@
     });
   })();
 
+const splashes = [
+    "also try meex.lol",
+    "post this мяу-мяу every week",
+    "виво х200 про в соло",
+    "Я ХЛЕБ РОВНО ОТРЕЗАЛ",
+    "welcome to my site",
+    "на айфон денег не хватило",
+    "пук пук я прёнулл☠️☠️",
+    "не ну эта патпiсон",
+    "завтра в 3",
+    "пахан когда разбан на кубере",
+    "удалите компьютер",
+    "also try amirtini.github.io",
+    "also try camp3rcraft.github.io",
+    "инт мейн говнюк",
+    "дайте мне денег",
+    "что тут писать?",
+    "best viewed on PC",
+    "паралеллограм☠️",
+    "пошли в рандом раш",
+    "это сплеш",
+    "i am steve 🗣️🗣️",
+    "але оп смотри",
+    "да ну не верю",
+    "кемпер не даёт идеи для сплешей",
+    "чел у тебя очепатка в спелше 🤓",
+    "pansan was here",
+    "🚽💨💨💨💥"
+];
+
 document.addEventListener('DOMContentLoaded', function() {
     const dash = document.querySelector('.dash');
     const navbar = document.querySelector('.navbar');
@@ -20,4 +50,24 @@ document.addEventListener('DOMContentLoaded', function() {
     dash.addEventListener('mouseout', () => {
         navbar.style.boxShadow = '0px 0px 5px #ffffff';
     });
-})
+
+    let audio = new Audio('meowmeow.mp3');
+    const meowmeow = document.getElementById("meowmeow")
+    meowmeow.addEventListener('click', () => {
+      if (!audio.paused) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+      audio.play();
+    });
+
+    const rareSplash = Math.random() < 0.01
+    const splash = document.querySelector(".splash");
+
+    if (rareSplash) {
+      splash.classList.add("rainbow-splash");
+      splash.textContent = "самый редкий сплеш";
+    } else {
+      splash.textContent = splashes[Math.floor(Math.random() * splashes.length)];
+    }
+});
